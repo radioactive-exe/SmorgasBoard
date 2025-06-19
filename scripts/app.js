76e00e94a9e8a84dd947e0782ddbd1dec748c01f
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-06-19 19:22:35 +0300

    Snapping. Currently working on the destination preview.

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,32 @@ const panels = [...document.querySelectorAll(".panel")];
 
 function moveElementWithinScreen(el, e, xInit, yInit, elLeftInit, elTopInit) {
         
-        el.style.setProperty("--x", clamp(elLeftInit + (e.pageX - xInit), 0, window.innerWidth - el.offsetWidth) + "px");
-        el.style.setProperty("--y", clamp(elTopInit + (e.pageY - yInit), 0, window.innerHeight - el.offsetHeight) + "px");
+    el.style.setProperty("--x", clamp(elLeftInit + (e.pageX - xInit), 0, window.innerWidth - el.offsetWidth) + "px");
+    el.style.setProperty("--y", clamp(elTopInit + (e.pageY - yInit), 0, window.innerHeight - el.offsetHeight) + "px");
 
 }
 
 function resizeElement(el, e, xInit, yInit, elWidthInit, elHeightInit) {
-    el.style.setProperty("--width", clamp(elWidthInit + e.pageX - xInit, getCssProperty(el, "--min-width"), window.innerWidth - el.offsetLeft) + "px");
-    el.style.setProperty("--height", clamp(elHeightInit + e.pageY - yInit, getCssProperty(el, "--min-height"), window.innerHeight - el.offsetTop) + "px");
+    el.style.setProperty("--width", clamp(elWidthInit + e.pageX - xInit, getNumericalValueOfCssProperty(el, "--min-width"), window.innerWidth - el.offsetLeft) + "px");
+    el.style.setProperty("--height", clamp(elHeightInit + e.pageY - yInit, getNumericalValueOfCssProperty(el, "--min-height"), window.innerHeight - el.offsetTop) + "px");
 }
 
+function snapElementToGrid(el) {
+    el.classList.add("snapping");
+    el.style.setProperty("--x", roundToNearest(getNumericalValueOfCssProperty(el, "--x"), window.innerWidth / 3) + "px");
+    el.style.setProperty("--y", roundToNearest(getNumericalValueOfCssProperty(el, "--y"), window.innerHeight / 3) + "px");
+
+    el.style.setProperty("--width", clamp(roundToNearest(getNumericalValueOfCssProperty(el, "--width"), window.innerWidth / 3), getNumericalValueOfCssProperty(el, "--min-width"), window.innerWidth - el.offsetLeft) + "px");
+    el.style.setProperty("--height", clamp(roundToNearest(getNumericalValueOfCssProperty(el, "--height"), window.innerHeight / 3), getNumericalValueOfCssProperty(el, "--min-height"), window.innerHeight - el.offsetTop) + "px");
+    
+    setTimeout(() => {
+        el.classList.remove("snapping");
+    }, getNumericalValueOfCssProperty(el, "transition-duration") * 1000);
+}
+
+function updateElementDestinationPreview(el) {
+    snapElementToGrid(el.querySelector(".final-preview"));
+}
 
 
 panels.forEach((i) => {
@@ -26,12 +42,14 @@ panels.forEach((i) => {
         const dragHandler = (e) => {
             e.preventDefault;
             moveElementWithinScreen(i, e, xInit, yInit, panelLeftInit, panelTopInit);
+            updateElementDestinationPreview(i);
         };
 
         const releaseHandler = (e) => {
             i.classList.remove("being-dragged");
             document.removeEventListener("mouseup", releaseHandler);
             document.removeEventListener("mousemove", dragHandler);
+            snapElementToGrid(i);
         }
 
         document.addEventListener("mouseup", releaseHandler);
@@ -39,6 +57,7 @@ panels.forEach((i) => {
         document.addEventListener("mousemove", dragHandler);
     })
     i.querySelector(".resize-handle").addEventListener("mousedown", (e) => {
+        i.classList.add("being-resized");
 
         const xInit = e.pageX;
         const yInit = e.pageY;
@@ -51,8 +70,10 @@ panels.forEach((i) => {
         };
 
         const releaseHandler = (e) => {
+            i.classList.remove("being-resized");
             document.removeEventListener("mouseup", releaseHandler);
             document.removeEventListener("mousemove", dragHandler);
+            snapElementToGrid(i);
         }
 
         document.addEventListener("mouseup", releaseHandler);
--- scripts/util.js
+++ scripts/util.js
@@ -5,3 +5,18 @@ function clamp(num, min, max) {
 function getCssProperty(el, property) {
     return window.getComputedStyle(el).getPropertyValue(property);
 }
+
+function roundToNearest(num, stepSize) {
+    let diminished = num / stepSize;
+    diminished = Math.round(diminished);
+    diminished *= stepSize;
+    return diminished;
+}
+
+function getNumericalValue(string) {
+    return string.replace(/\D+$/g, "")
+}
+
+function getNumericalValueOfCssProperty(el, property) {
+    return getNumericalValue(getCssProperty(el, property));
+}
