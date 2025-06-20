a802b4beb5294bcf471b4cdbd301822656ec65d7
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-06-21 00:48:24 +0300

    Preview works for multiple panels, and preview transitioning also works. All is well

--- scripts/app.js
+++ scripts/app.js
@@ -8,21 +8,21 @@ function moveElementWithinScreen(el, e, xInit, yInit, elLeftInit, elTopInit) {
 }
 
 function resizeElement(el, e, xInit, yInit, elWidthInit, elHeightInit) {
-    el.style.setProperty("--width", clamp(elWidthInit + e.pageX - xInit, getNumericalValueOfCssProperty(el, "--min-width"), window.innerWidth - el.offsetLeft) + "px");
-    el.style.setProperty("--height", clamp(elHeightInit + e.pageY - yInit, getNumericalValueOfCssProperty(el, "--min-height"), window.innerHeight - el.offsetTop) + "px");
+    el.style.setProperty("--width", clamp(elWidthInit + e.pageX - xInit, getNormalisedCssPropertyValue(el, "--min-width"), window.innerWidth - el.offsetLeft) + "px");
+    el.style.setProperty("--height", clamp(elHeightInit + e.pageY - yInit, getNormalisedCssPropertyValue(el, "--min-height"), window.innerHeight - el.offsetTop) + "px");
 }
 
 function snapElementToGrid(el, source) {
     el.classList.add("snapping");
-    el.style.setProperty("--x", roundToNearest(getNumericalValueOfCssProperty(source, "--x"), window.innerWidth / 3) + "px");
-    el.style.setProperty("--y", roundToNearest(getNumericalValueOfCssProperty(source, "--y"), window.innerHeight / 3) + "px");
+    el.style.setProperty("--x", roundToNearest(getNormalisedCssPropertyValue(source, "--x"), window.innerWidth / 3) + "px");
+    el.style.setProperty("--y", roundToNearest(getNormalisedCssPropertyValue(source, "--y"), window.innerHeight / 3) + "px");
 
-    el.style.setProperty("--width", clamp(roundToNearest(getNumericalValueOfCssProperty(source, "--width"), window.innerWidth / 3), getNumericalValueOfCssProperty(source, "--min-width"), window.innerWidth - source.offsetLeft) + "px");
-    el.style.setProperty("--height", clamp(roundToNearest(getNumericalValueOfCssProperty(source, "--height"), window.innerHeight / 3), getNumericalValueOfCssProperty(source, "--min-height"), window.innerHeight - source.offsetTop) + "px");
+    el.style.setProperty("--width", clamp(roundToNearest(getNormalisedCssPropertyValue(source, "--width"), window.innerWidth / 3), getNormalisedCssPropertyValue(source, "--min-width"), window.innerWidth - source.offsetLeft) + "px");
+    el.style.setProperty("--height", clamp(roundToNearest(getNormalisedCssPropertyValue(source, "--height"), window.innerHeight / 3), getNormalisedCssPropertyValue(source, "--min-height"), window.innerHeight - source.offsetTop) + "px");
     
     setTimeout(() => {
         el.classList.remove("snapping");
-    }, getNumericalValueOfCssProperty(el, "transition-duration") * 1000);
+    }, getNormalisedCssPropertyValue(el, "transition-duration"));
 }
 
 function updateElementDestinationPreview(el) {
@@ -31,14 +31,24 @@ function updateElementDestinationPreview(el) {
 
 
 panels.forEach((i) => {
+
+    i.style.setProperty("--x", "0px");
+    i.style.setProperty("--y", "0px");
+    i.style.setProperty("--width", "var(--min-width)");
+    i.style.setProperty("--height", "var(--min-height)");
+
+    const preview = document.createElement("div");
+    preview.classList.add("final-preview");
+    
     i.querySelector(".drag-handle").addEventListener("mousedown", (e) => {
         i.classList.add("being-dragged");
-
+        i.parentElement.prepend(preview);
+        
         const xInit = e.pageX;
         const yInit = e.pageY;
         const panelLeftInit = i.offsetLeft;
         const panelTopInit = i.offsetTop;
-
+        
         const dragHandler = (e) => {
             e.preventDefault;
             moveElementWithinScreen(i, e, xInit, yInit, panelLeftInit, panelTopInit);
@@ -49,7 +59,10 @@ panels.forEach((i) => {
             i.classList.remove("being-dragged");
             document.removeEventListener("mouseup", releaseHandler);
             document.removeEventListener("mousemove", dragHandler);
-            snapElementToGrid(i, i);
+            snapElementToGrid(i, i);            
+            setTimeout(() => {
+                i.parentElement.removeChild(preview);
+            }, getNormalisedCssPropertyValue(preview, "transition-duration"));
         }
 
         document.addEventListener("mouseup", releaseHandler);
@@ -58,6 +71,7 @@ panels.forEach((i) => {
     })
     i.querySelector(".resize-handle").addEventListener("mousedown", (e) => {
         i.classList.add("being-resized");
+        i.parentElement.prepend(preview);
 
         const xInit = e.pageX;
         const yInit = e.pageY;
@@ -67,10 +81,11 @@ panels.forEach((i) => {
         const dragHandler = (e) => {
             e.preventDefault;
             resizeElement(i, e, xInit, yInit, panelWidthInit, panelHeightInit);
-            updateElementDestinationPreview(i)
+            updateElementDestinationPreview(i);
         };
 
         const releaseHandler = (e) => {
+            i.parentElement.removeChild(preview);
             i.classList.remove("being-resized");
             document.removeEventListener("mouseup", releaseHandler);
             document.removeEventListener("mousemove", dragHandler);
--- scripts/util.js
+++ scripts/util.js
@@ -17,6 +17,31 @@ function getNumericalValue(string) {
     return string.replace(/\D+$/g, "")
 }
 
-function getNumericalValueOfCssProperty(el, property) {
+function getCssPropertyValue(el, property) {
     return getNumericalValue(getCssProperty(el, property));
+}
+
+function getNormalisedCssPropertyValue(el, property) {
+
+    if (getCssProperty(el, property) == "0") return 0; // No unit is often specified with 0
+
+    const propertyUnit = /[a-zA-Z]+/.exec(getCssProperty(el, property))[0];
+    
+    switch (propertyUnit) {
+        case "rem":
+            return getCssPropertyValue(el, property) * 10;
+            break;
+        case "px":
+            return getCssPropertyValue(el, property);
+            break;
+        case "s":
+            return getCssPropertyValue(el, property) * 1000;
+            break;
+        case "ms":
+            return getCssPropertyValue(el, property);
+            break;
+        default:
+            return getCssPropertyValue(el, property);
+
+    }
 }
