f222769d27106653c70967fd9ffe618ccc7d37b4
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-05 16:28:34 +0300

    Backend operational?

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,6 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend/public", "index.html"))
+    res.send("Hello")
 );
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
