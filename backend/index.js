387d4e90f32a6e8ac0657e5efb351c511a421096
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-05 14:51:08 +0300

    Fix?

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,6 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend/public", "index.html"))
+    res.sendFile(path.join(process.cwd(), "frontend", "public", "index.html"))
 );
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
