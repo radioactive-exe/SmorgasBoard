4e3cc50a71662a4bc0402b5b7888c9b24b3aac4e
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-05 14:14:00 +0300

    Absolute path.

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile('../frontend/public/index.html'))
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../frontend/public", "index.html"))
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
