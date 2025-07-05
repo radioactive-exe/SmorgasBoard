a3b6a969b94ea6582b22378bcb58d64fc6019d6f
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-05 14:36:45 +0300

    Vercel. Again.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "../frontend/public", "index.html"))
