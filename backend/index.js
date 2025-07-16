2160d8a70e4b2670a08d7cdbb9b34c5c391fc157
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-16 18:32:29 +0300

    Backend testing

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ app.use(cors({ origin: true, credentials: true }));
 app.get("/", (req, res) => res.send("Hello"));
 
 app.get("api/definitions/templates/default", (req, res) => {
-    res.sendFile("./definitions/templates/default.html");
+    // res.sendFile("./definitions/templates/default.html");
+    res.send("No");
 });
 app.get("api/definitions/templates/notepad", (req, res) => {
     res.sendFile("./definitions/templates/notepad.html");
