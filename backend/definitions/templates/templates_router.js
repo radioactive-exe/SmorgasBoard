dd0c8ab3d7d0b58a2607918840934526407a9470
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-16 19:08:57 +0300

    Backend Routing

---
+++ backend/definitions/templates/templates_router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path")
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Here, all the default Panel Templates are requested");
+});
+
+router.get("/:template", (req, res) => {
+    res.sendFile(`${req.params.template}.html`);
+})
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const { get } = require("http");
+const templatesRouter = require("./definitions/templates/templates_router.js");
 
 const app = express();
 const port = 3000;
 
 app.use(cors({ origin: true, credentials: true }));
+app.use("api/definitions/templates/", templatesRouter);
 
 app.get("/", (req, res) => res.send("Hello"));
 
