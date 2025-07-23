7fede9360a8a1cc9f2f982077e2186ee3718c3f7
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-23 22:21:40 +0300

    Backend uses TS now.

--- backend/definitions/definitions_router.ts
+++ backend/definitions/definitions_router.ts
@@ -1,13 +1,15 @@
-const express = require("express");
-const path = require("path")
+const express = require('express');
+
+import { Request, Response } from "express";
+
 const router = express.Router();
 const fs = require('fs');
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
     res.send("Here, all the Definitions are requested and sent");
 });
 
-router.get("/:panel", (req, res) => {
+router.get("/:panel", (req: Request , res: Response) => {
     var templateHtml = fs.readFileSync(__dirname + `/templates/${req.params.panel}.html`)
     res.send({
         panel_type: req.params.panel,
--- backend/index.ts
+++ backend/index.ts
@@ -3,12 +3,14 @@ const path = require("path");
 const cors = require("cors");
 const templatesRouter = require("./definitions/definitions_router.js");
 
+import { Request, Response } from "express";
+
 const app = express();
 const port = 3000;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use("/definitions/panels/", templatesRouter);
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/", (req: Request, res: Response) => res.sendFile(__dirname + "/index.html"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
