be28d14bb334ff0f6a768425859bfe007547cdc0
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-07-23 22:28:59 +0300

    Fixed backend build commands and typescript directories

--- backend/index.ts
+++ backend/index.ts
@@ -11,6 +11,6 @@ const port = 3000;
 app.use(cors({ origin: true, credentials: true }));
 app.use("/definitions/panels/", templatesRouter);
 
-app.get("/", (req: Request, res: Response) => res.sendFile(__dirname + "/index.html"));
+app.get("/", (req: Request, res: Response) => res.sendFile(__dirname + "/../index.html"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
