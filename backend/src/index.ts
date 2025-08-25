967f02f76a0b431d3666484849b72001dd341b5d
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-08-25 20:12:52 +0300

    A proper CORS whitelist is now in effect. This should no longer give me constant CORS errors especially from localhost/127.0.0.1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,37 @@
 import { cors, express, path } from "./declarations.js";
 
-const definitionsRouter : express.Router = require("./routers/definitions_router");
+const definitionsRouter: express.Router = require("./routers/definitions_router");
 
 const app = express();
 const port = 3000;
+const allowedOrigins: string[] = [
+    "https://smorgasboard.vercel.app/",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+    "http://127.0.0.1:3002",
+    "http://127.0.0.1:3003",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:3003",
+];
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(
+                    new Error(
+                        "Domain not White-listed by CORS. Please contact github.com/Radioactive_exe if this is an issue"
+                    )
+                );
+            }
+        },
+        credentials: true,
+    })
+);
 
 app.use("/definitions/", definitionsRouter);
 // app.use("/api/", apiRouter);
@@ -14,4 +40,8 @@ app.get("/", (req: express.Request, res: express.Response) => {
     res.sendFile(path.resolve(__dirname + "/../index.html"));
 });
 
-app.listen(port, () => console.log(`SmorgasBoard listening on SmorgasPort ${port}! (Get it? Cuz... Smorg- eh whatever)`));
+app.listen(port, () =>
+    console.log(
+        `SmorgasBoard listening on SmorgasPort ${port}! (Get it? Cuz... Smorg- eh whatever)`
+    )
+);
