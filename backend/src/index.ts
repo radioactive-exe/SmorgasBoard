3c3befd2442ac3d5c7b2489f699082706e618164
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-09-04 21:09:47 +0300

    Seems to be a bug, these might be fixes to fix things in production.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
+require("dotenv").config();
+
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 import { cors, express, path } from "./declarations.js";
 
-require("dotenv").config();
 
 const supabaseUrl: string = process.env.SUPABASE_URL ?? "";
 const supabaseKey: string = process.env.SUPABASE_ANON_KEY ?? "";
