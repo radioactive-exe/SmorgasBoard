f690f5ed6a91a0b36a39d3046a4dff8e120dd522
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-09-04 21:01:30 +0300

    This is working in nodemon but not in production. This does not make much sense.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,7 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 import { cors, express, path } from "./declarations.js";
 
-require("dotenv").config({
-    path: "../"
-});
+require("dotenv").config();
 
 const supabaseUrl: string = process.env.SUPABASE_URL ?? "";
 const supabaseKey: string = process.env.SUPABASE_ANON_KEY ?? "";
