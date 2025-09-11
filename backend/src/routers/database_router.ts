e5ba1ac3cacf024f4b1dd0cf7f3ccce95aedf346
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-09-11 21:15:00 +0300

    .

--- backend/src/routers/database_router.ts
+++ backend/src/routers/database_router.ts
@@ -58,7 +58,7 @@ databaseRouter.get(
                     apiKey: process.env.SUPABASE_KEY ?? "",
                     Authorization: req.headers.authorization ?? "",
                 },
-                body: req.body,
+                body: req.body.json(),
             }
         );
 
