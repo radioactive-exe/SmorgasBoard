9f1d0dc382a9eea2c4a3df32f8f8d12feec01794
Radioactive.exe <84581607+radioactive-exe@users.noreply.github.com>
2025-12-05 21:06:57 +0200

    Reverted all changes.

--- backend/src/routers/definitions_subrouters/templates_router.ts
+++ backend/src/routers/definitions_subrouters/templates_router.ts
@@ -39,8 +39,10 @@ const _templateHandler = templatesRouter.get(
         }
 
         // ? Obtain the (potential) location for the template file
-        const templateLocation = `${__dirname}${process.env.DEFINITIONS_RELATIVE_PATH}/templates/${req.params.panel}.html`;
-        console.log(templateLocation);
+        const templateLocation = path.join(
+            __dirname
+                + `/../../../definitions/templates/${req.params.panel}.html`,
+        );
 
         // ? In case the panel parameter for which the template is requested does not
         // ? have an implemented template file
